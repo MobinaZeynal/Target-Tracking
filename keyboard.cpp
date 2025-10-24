db7d438f5233defeae7d4e0034a1e728d476c75d
zeynal <mobinazeynal@gmail.com>
2025-10-24 18:07:02 +0330

    add focus and zoom
    Signed-off-by: zeynal <mobinazeynal@gmail.com>

--- keyboard.cpp
+++ keyboard.cpp
@@ -5,20 +5,48 @@ Keyboard::Keyboard(Command *cmd, QObject *parent)
     : QObject(parent), m_cmd(cmd)
 {
     speed = 63; // سرعت پیش فرض
+
     ptzTimer = new QTimer(this);
-    ptzTimer->setInterval(50); // هر 50 میلی‌ثانیه فرمان PTZ ارسال شود برای روانی بهتر
+    ptzTimer->setInterval(70);
 
     connect(ptzTimer, &QTimer::timeout, [=]() {
-        if (m_cmd && currentAction != -1) {
-            switch (currentAction) {
-                case 0: m_cmd->sendPtz(channel, 0, 0, speed); break; // Up
-                case 1: m_cmd->sendPtz(channel, 1, 0, speed); break; // Down
-                case 2: m_cmd->sendPtz(channel, 2, speed, 0); break; // Left
-                case 3: m_cmd->sendPtz(channel, 3, speed, 0); break; // Right
-            }
+        if (currentActionId != -1 && m_cmd) {
+            m_cmd->sendPtz(currentChannel, currentActionId, speed, speed);
         }
     });
 }
+
+void Keyboard::startAction(int actionId, int ch)
+{
+    currentActionId = actionId;
+    currentChannel = ch;
+    ptzTimer->start();
+}
+
+void Keyboard::stopAction()
+{
+    ptzTimer->stop();
+
+    if (!m_cmd) {
+        currentActionId = -1;
+        return;
+    }
+
+    int stopActionId = 8; // default: pan/tilt stop
+
+    if (currentActionId == 9 || currentActionId == 10) {
+        stopActionId = 29;
+    }
+    else if (currentActionId == 11 || currentActionId == 12) {
+        stopActionId = 30;
+    }
+
+    m_cmd->sendPtz(currentChannel, stopActionId, 0, 0);
+    currentActionId = -1;
+}
+
+
+
 
 bool Keyboard::eventFilter(QObject *obj, QEvent *event)
 {
@@ -26,38 +54,68 @@ bool Keyboard::eventFilter(QObject *obj, QEvent *event)
 
     if (event->type() == QEvent::KeyPress) {
         QKeyEvent *key = static_cast<QKeyEvent*>(event);
-
-        if (key->isAutoRepeat()) return true; // نادیده گرفتن تکرار خودکار سیستم
+        if (key->isAutoRepeat()) return true;
 
         switch (key->key()) {
-            case Qt::Key_Up: currentAction=0; ptzTimer->start(); break;
-            case Qt::Key_Down: currentAction=1; ptzTimer->start(); break;
-            case Qt::Key_Left: currentAction=2; ptzTimer->start(); break;
-            case Qt::Key_Right: currentAction=3; ptzTimer->start(); break;
-            default: return QObject::eventFilter(obj, event);
+            // PTZ Move
+            case Qt::Key_Up:           startAction(0, 0); return true; // Up RGB
+            case Qt::Key_Down:         startAction(1, 0); return true; // Down RGB
+            case Qt::Key_Left:         startAction(2, 0); return true; // Left RGB
+            case Qt::Key_Right:        startAction(3, 0); return true; // Right RGB
+
+            case Qt::Key_7:            startAction(4, 0); return true; // Upper Left RGB
+            case Qt::Key_1:            startAction(5, 0); return true; // Lower Left RGB
+            case Qt::Key_9:            startAction(6, 0); return true; // Upper Right RGB
+            case Qt::Key_3:            startAction(7, 0); return true; // Lower Right RGB
+
+            // Zoom RGB
+            case Qt::Key_Q:            startAction(9, 0); return true;  // Zoom +
+            case Qt::Key_W:            startAction(10, 0); return true; // Zoom -
+
+            // Zoom IR
+            case Qt::Key_E:            startAction(9, 1); return true;  // Zoom +
+            case Qt::Key_R:            startAction(10, 1); return true; // Zoom -
+
+            // Focus RGB
+            case Qt::Key_A:            startAction(12, 0); return true; // Focus +
+            case Qt::Key_S:            startAction(11, 0); return true; // Focus -
+
+            // Focus IR
+            case Qt::Key_D:            startAction(12, 1); return true; // Focus +
+            case Qt::Key_F:            startAction(11, 1); return true; // Focus -
+
+            default:
+                return QObject::eventFilter(obj, event);
         }
-        return true;
     }
 
     if (event->type() == QEvent::KeyRelease) {
         QKeyEvent *key = static_cast<QKeyEvent*>(event);
-
         if (key->isAutoRepeat()) return true;
 
         switch (key->key()) {
-            case Qt::Key_Up:
-            case Qt::Key_Down:
-            case Qt::Key_Left:
-            case Qt::Key_Right:
-                ptzTimer->stop();
-                currentAction = -1;
-                m_cmd->sendPtz(channel, 8, 0, 0); // Stop PTZ
-                break;
-            default:
-                return QObject::eventFilter(obj, event);
+            case Qt::Key_Up: case Qt::Key_Down: case Qt::Key_Left: case Qt::Key_Right:
+            case Qt::Key_7: case Qt::Key_1: case Qt::Key_9: case Qt::Key_3:
+            case Qt::Key_Q: case Qt::Key_W: case Qt::Key_E: case Qt::Key_R:
+            case Qt::Key_A: case Qt::Key_S: case Qt::Key_D: case Qt::Key_F:
+                stopAction();
+                return true;
         }
-        return true;
     }
 
     return QObject::eventFilter(obj, event);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- keyboard.h
+++ keyboard.h
@@ -1,9 +1,44 @@
+//#ifndef KEYBOARD_H
+//#define KEYBOARD_H
+
+//#include <QObject>
+//#include <QTimer>
+//#include "command.h" // کلاس Command که sendPtz را دارد
+
+//class Keyboard : public QObject
+//{
+//    Q_OBJECT
+//public:
+//    explicit Keyboard(Command *cmd, QObject *parent = nullptr);
+
+//    bool eventFilter(QObject *obj, QEvent *event) override;
+
+//    void setSpeed(int s) { speed = s; }
+//    int getSpeed() const { return speed; }
+
+//    void setChannel(int ch) { channel = ch; }
+//    int getChannel() const { return channel; }
+
+//private:
+//    Command *m_cmd;
+//    QTimer *ptzTimer;
+//    int speed;
+//    int channel = 0;        // کانال دوربین (RGB=0، IR=1)
+//    int currentAction = -1; // 0=Up,1=Down,2=Left,3=Right
+//};
+
+//#endif // KEYBOARD_H
+
+
+
+
+
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
 #include <QObject>
 #include <QTimer>
-#include "command.h" // کلاس Command که sendPtz را دارد
+#include "command.h"
 
 class Keyboard : public QObject
 {
@@ -16,15 +51,16 @@ public:
     void setSpeed(int s) { speed = s; }
     int getSpeed() const { return speed; }
 
-    void setChannel(int ch) { channel = ch; }
-    int getChannel() const { return channel; }
+private:
+    void startAction(int actionId, int ch);
+    void stopAction();
 
 private:
     Command *m_cmd;
     QTimer *ptzTimer;
     int speed;
-    int channel = 0;        // کانال دوربین (RGB=0، IR=1)
-    int currentAction = -1; // 0=Up,1=Down,2=Left,3=Right
+    int currentActionId = -1;
+    int currentChannel = 0;
 };
 
 #endif // KEYBOARD_H
