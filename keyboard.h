30fb087b9e6a9e240a6cf60045250fa51704e7c2
zeynal <mobinazeynal@gmail.com>
2025-10-24 18:31:21 +0330

    clean code
    Signed-off-by: zeynal <mobinazeynal@gmail.com>

--- keyboard.h
+++ keyboard.h
@@ -1,38 +1,3 @@
-//#ifndef KEYBOARD_H
-//#define KEYBOARD_H
-
-//#include <QObject>
-//#include <QTimer>
-//#include "command.h" // کلاس Command که sendPtz را دارد
-
-//class Keyboard : public QObject
-//{
-//    Q_OBJECT
-//public:
-//    explicit Keyboard(Command *cmd, QObject *parent = nullptr);
-
-//    bool eventFilter(QObject *obj, QEvent *event) override;
-
-//    void setSpeed(int s) { speed = s; }
-//    int getSpeed() const { return speed; }
-
-//    void setChannel(int ch) { channel = ch; }
-//    int getChannel() const { return channel; }
-
-//private:
-//    Command *m_cmd;
-//    QTimer *ptzTimer;
-//    int speed;
-//    int channel = 0;        // کانال دوربین (RGB=0، IR=1)
-//    int currentAction = -1; // 0=Up,1=Down,2=Left,3=Right
-//};
-
-//#endif // KEYBOARD_H
-
-
-
-
-
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
